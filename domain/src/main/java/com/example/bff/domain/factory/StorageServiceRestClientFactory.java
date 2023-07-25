266999214892b1f808a2516b29892a384956c330
user <danez7000@gmail.com>
2023-07-25 11:47:45 +0300

    Initial commit

---
+++ domain/src/main/java/com/example/bff/domain/factory/StorageServiceRestClientFactory.java
@@ -0,0 +1,25 @@
+package com.example.bff.domain.factory;
+
+import com.example.storageservice.restexport.StorageServiceRestClient;
+import com.example.zoostore.restexport.ZooStoreRestClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class StorageServiceRestClientFactory {
+    @Bean
+    StorageServiceRestClient getRestExportClient() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        return Feign.builder()
+                .encoder(new JacksonEncoder(objectMapper))
+                .decoder(new JacksonDecoder(objectMapper))
+                .target(StorageServiceRestClient.class, "http://localhost:8081");
+    }
+}
---
+++ domain/src/main/java/com/example/bff/domain/factory/ZooStoreRestClientFactory.java
@@ -0,0 +1,25 @@
+package com.example.bff.domain.factory;
+import com.example.zoostore.restexport.ZooStoreRestClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class ZooStoreRestClientFactory {
+
+    @Bean
+    ZooStoreRestClient getRestExportClient() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        return Feign.builder()
+                .encoder(new JacksonEncoder(objectMapper))
+                .decoder(new JacksonDecoder(objectMapper))
+                .target(ZooStoreRestClient.class, "http://localhost:8080");
+    }
+
+}
